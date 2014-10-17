7a22b35e61b4fdfc743808644ae3c91dad53c1c9
x_caohj <x_caohj@hyron-js.com>
2014-10-17 09:13:59 +0800

    rr

---
+++ Test/Test.MVCSite/Controllers/TestJsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test.MVCSite.Controllers
+{
+    public class TestJsController : Controller
+    {
+        // GET: TestJs
+        public ActionResult ZipImg()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Save(byte[] formFile)
+        {
+            System.IO.File.WriteAllBytes(@"c:\a.png",formFile);
+            return new JsonResult(){};
+        }
+    }
+}
