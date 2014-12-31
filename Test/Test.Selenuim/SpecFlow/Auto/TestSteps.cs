f427560edc5fa0eae2fd93e4b0a0f280faedbe76
x_caohj <x_caohj@hyron-js.com>
2014-12-31 13:55:53 +0800

    tt

---
+++ Test/Test.Selenuim/SpecFlow/Auto/TestSteps.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+using NUnit.Framework;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using Selenium;
+using TechTalk.SpecFlow;
+
+namespace Test.Selenuim.SpecFlow.Auto
+{
+    [Binding]
+    public class StepDefinitions
+    {
+        private readonly IWebDriver _driver = new FirefoxDriver();
+        private readonly  ISelenium  _selenium = null;
+        public StepDefinitions()
+        {
+             _selenium = new WebDriverBackedSelenium(_driver, "http://www.baidu.com/");
+            _selenium.Start();
+             _selenium.Open("/");
+            //_driver.Navigate().GoToUrl();
+        }
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            _selenium.Type("id=kw", "abc");
+            //IWebElement query = _driver.FindElement(By.Id("kw"));
+            //query.SendKeys(query.Text + p0.ToString(CultureInfo.InvariantCulture));
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.AreEqual(120, p0);
+        }
+    }
+}
---
+++ Test/TestApi/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var api = new ApiTest {Request = {Method = HttpMethod.Get}};
+            api.Get();
+        }
+    }
+}
