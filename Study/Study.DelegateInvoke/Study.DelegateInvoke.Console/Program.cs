d094f45658a4e848caf1f89d05484f0cc58970da
diymine <diymine@163.com>
2016-11-12 23:13:56 +0800

    study delegate

---
+++ Study/Study.DelegateInvoke/Study.DelegateInvoke.Console/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Study.DelegateInvoke
+{
+    class Program
+    {
+        private static int NewTask(int ms)
+        {
+
+            Console.WriteLine("任务开始" + DateTime.Now);
+
+            Thread.Sleep(ms);
+
+            Random random = new Random();
+
+            int n = random.Next(5000);
+
+            Console.WriteLine("任务完成" + DateTime.Now);
+
+            return n;
+
+        }
+
+        private delegate int NewTaskDelegate(int ms);
+
+
+
+
+        static void Main(string[] args)
+        {
+            NewTaskDelegate task = NewTask;
+
+            IAsyncResult asyncResult = task.BeginInvoke(5000, null, null);
+
+            // EndInvoke方法将被阻塞2秒
+            Thread.Sleep(2000);
+            Console.WriteLine("主线程时间1：" + DateTime.Now);
+            //while (!asyncResult.IsCompleted)
+            //{
+            //    Console.Write("*");
+            //}
+            while (!asyncResult.AsyncWaitHandle.WaitOne(2000, false))
+            {
+
+                Console.Write("*");
+            }
+            Console.WriteLine("主线程时间2：" + DateTime.Now);
+            int result = task.EndInvoke(asyncResult);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Study/Study.Winform/Study.Winform.GridView/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Study.Winform.GridView
+{
+    public class User
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+
+        
+    }
+    public partial class Form1 : Form
+    {
+        private BindingSource masterBindingSource = new BindingSource();
+        private BindingSource detailsBindingSource = new BindingSource();
+        private BindingSource masterBindingSource2 = new BindingSource();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView3.DataSource = masterBindingSource2;
+            // Bind the DataGridView controls to the BindingSource
+
+            // components and load the data from the database.
+
+            dataGridView1.DataSource = masterBindingSource;
+
+            dataGridView2.DataSource = detailsBindingSource;
+
+            GetData();
+
+            // Resize the master DataGridView columns to fit the newly loaded data.
+
+            dataGridView1.AutoResizeColumns();
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                column.Width = 80;
+            }
+
+            dataGridView1.Columns[0].Frozen = true;
+            dataGridView1.Columns[1].Frozen = true;
+            // Configure the details DataGridView so that its columns automatically
+
+            // adjust their widths when the data changes.
+
+            dataGridView2.AutoSizeColumnsMode =
+
+                DataGridViewAutoSizeColumnsMode.AllCells;
+        }
+
+        private void GetData()
+        {
+            var dataSet = new DataSet();
+            var table = new DataTable("Customer");
+            table.Columns.Add(new DataColumn("a"));
+            table.Columns.Add(new DataColumn("b"));
+            table.Columns.Add(new DataColumn("c"));
+            table.Columns.Add(new DataColumn("d"));
+            table.Columns.Add(new DataColumn("e"));
+            table.Columns.Add(new DataColumn("f"));
+            table.Columns.Add(new DataColumn("g"));
+            table.Columns.Add(new DataColumn("h"));
+            table.Rows.Add(new object[] { "aa", "bb", "cc", "dd", "ee", "ff", "gg", "hh" });
+            table.Rows.Add(new object[] { "cc", "dd1", "cc", "dd", "ee", "ff", "gg", "hh" });
+            var table1 = new DataTable("Order");
+            table1.Columns.Add(new DataColumn("c"));
+            table1.Columns.Add(new DataColumn("d"));
+            table1.Rows.Add(new object[] { "aa", "bb" });
+            table1.Rows.Add(new object[] { "cc", "ddddddff" });
+            dataSet.Tables.Add(table);
+            dataSet.Tables.Add(table1);
+            var relation = new DataRelation("relationTest", table.Columns["a"], table1.Columns["c"]);
+            dataSet.Relations.Add(relation);
+            masterBindingSource.DataSource = dataSet;
+            masterBindingSource.DataMember = "Customer";
+            detailsBindingSource.DataSource = masterBindingSource;
+            detailsBindingSource.DataMember = "relationTest";
+
+            masterBindingSource2.DataSource = new List<User>()
+            {
+                new User(){Code = "11",Name = "22"},
+                new User(){Code = "33",Name = "22"},
+            };
+
+
+        }
+    }
+}
